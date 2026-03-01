f20b2e9b898adc33ea408d45d815e817ed19b858
jahnavis <jahnavi.singampalli@global-csg.com>
2026-03-01 22:08:09 +0530

    changedtext

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import APP_NAME
 def run():
     print(f"Welcome to {APP_NAME}")
     
-    name = input("Enter your name: ")
+    name = input("Enter your name and the details: ")
     greet_user(name)
     
     result = add_numbers(5, 3)
